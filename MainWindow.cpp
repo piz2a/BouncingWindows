70b5af3adb959cb0304168af63c89636104f88e8
Ahn Ji-ho <43025513+piz2a@users.noreply.github.com>
2023-07-20 14:27:51 +0900

    Update MainWindow.cpp

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -13,81 +13,30 @@ void MainWindow::create(HWND hwnd_, HINSTANCE g_hInst_) {
 
 
 void MainWindow::TouchProc(const std::vector<MovingWindow*>& vMovingWindow, int nCode, WPARAM wParam, LPARAM lParam) {
-    /*HWND photoHwnd = CreateWindow("PhotoWindow", "¿ì¸®µéÀÇ Ãß¾ï", WS_OVERLAPPEDWINDOW,
-                                  CW_USEDEFAULT, CW_USEDEFAULT, 800, 600, (HWND)nullptr, (HMENU)nullptr, (HINSTANCE)nullptr, nullptr);
-    ShowWindow(photoHwnd, SW_SHOWNORMAL);*/
     for (MovingWindow* movingWindow : vMovingWindow) {
         if (movingWindow->velocityCmp(1000) == 1) continue;
         movingWindow->multiplyVelocity(20.0);
         movingWindow->setRestitution(0.9);
     }
-/*
-    DWORD dwThreadID;
-    HANDLE hThread = CreateThread(nullptr, 0, [vMovingWindow](LPVOID lpParam) WINAPI -> DWORD {
-        Sleep(3000);
-        for (MovingWindow movingWindow : vMovingWindow) movingWindow.setRestitution(1.0);
-        return 0;
-    }, nullptr, 0, &dwThreadID);
-    if (hThread == nullptr) ExitProcess(3);*/
 }
 
 
 void MainWindow::onWMCreate(UINT iMessage, WPARAM wParam, LPARAM lParam) {
-    /*
-    HWND hChildWnd = CreateWindow(
-            "button",        		// À©µµ¿ì Å¬·¡½º ÀÌ¸§
-            "¹öÆ°",	                // À©µµ¿ì Å¸ÀÌÆ²
-            WS_CHILD | WS_VISIBLE, 	// À©µµ¿ì ½ºÅ¸ÀÏ
-            0,                      // À©µµ¿ì º¸ÀÏ ¶§ x ÁÂÇ¥
-            0,                      // À©µµ¿ì º¸ÀÏ ¶§ y ÁÂÇ¥
-            windowWidth,            // À©µµ¿ì Æø
-            100,                    // À©µµ¿ì ³ôÀÌ
-            hwnd,         	        // ºÎ¸ð À©µµ¿ì
-            (HMENU)ID_OK_BTN,       // ÄÁÆ®·Ñ ID
-            g_hInst,           	    // ÀÎ½ºÅÏ½º ÇÚµé
-            (LPVOID)nullptr);       // ¿©ºÐÀÇ µ¥ÀÌÅÍ
-
-    if (!hChildWnd) return -1;*/
-    /*hBitmap = (HBITMAP) LoadImage(g_hInst, "C:/Users/jiho/Desktop/gift.jpg", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
-    std::cout << "bitmap: " << hBitmap << std::endl;*/
 }
 
 
 void MainWindow::onWMPaint(UINT iMessage, WPARAM wParam, LPARAM lParam) {
-    LPCTSTR text = "¾Æ¹« Å°³ª ´­·¯º¸¼¼¿ä";
+    LPCTSTR text = "ì•„ë¬´ í‚¤ë‚˜ ëˆŒëŸ¬ë³´ì„¸ìš”";
     PAINTSTRUCT ps;
     HDC hdc = BeginPaint(hwnd, &ps);
     TextOut(hdc, 0, 0, text, lstrlen(text));
-
-    /*
-    BITMAP bitmap;
-    HDC hdcMem = CreateCompatibleDC(hdc);
-    HGDIOBJ oldBitmap = SelectObject(hdcMem, hBitmap);
-
-    GetObject(hBitmap, sizeof(bitmap), &bitmap);
-    BitBlt(hdc, 0, 0, bitmap.bmWidth, bitmap.bmHeight, hdcMem, 0, 0, SRCCOPY);
-
-    SelectObject(hdcMem, oldBitmap);
-    DeleteDC(hdcMem);
-     */
-
     EndPaint(hwnd, &ps);
 }
 
 
 void MainWindow::onWMCommand(UINT iMessage, WPARAM wParam, LPARAM lParam) {
-    /*
-    if (LOWORD(wParam) == ID_OK_BTN)
-    {
-        // SetWindowPos(hWnd, 0, 500, 500, 300, 300, SWP_NOREDRAW | SWP_NOZORDER);
-        // SetWindowLongPtr(hWnd, GWL_STYLE, dwNewStyles);
-        // RedrawWindow(hWnd, 0, 0, RDW_INVALIDATE | RDW_FRAME);
-        MessageBox(hwnd, "¹öÆ°ÀÌ Å¬¸¯µÇ¾ú½À´Ï´Ù.", "¸Þ½ÃÁö", MB_OK);
-    }
-    */
 }
 
 
 void MainWindow::onWMDestroy(UINT iMessage, WPARAM wParam, LPARAM lParam) {
-    // DeleteObject(hBitmap);
 }
